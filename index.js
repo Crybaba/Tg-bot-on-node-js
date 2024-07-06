f4650bb67305aba8048613f7302ce9f92974bce2
Crybaby <goodegod2@gmail.com>
2024-07-06 20:22:19 +0800

    minor fix

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ server.on('request', (request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     response.end('<h1>Привет, октагон!</h1>');
 });
-server.listen(3000, () => {"Сервер начал прослушивание запросов на порту 3000"});
+server.listen(3000, "127.0.0.1" ,() => {console.log("Сервер начал прослушивание запросов на порту 3000")});
