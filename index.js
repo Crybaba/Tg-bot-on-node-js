a7573176103795c3d1996c1cbfe54df358d7113c
Crybaby <goodegod2@gmail.com>
2024-07-10 18:46:52 +0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '7488860731:AAEOQIjOx720ea2zprp9Ymix5l-DnifQ2ws';
 
+//bot link https://t.me/oktagon_test_bot
+
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
